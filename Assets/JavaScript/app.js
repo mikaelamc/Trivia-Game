f1bf7277eb7145278ee0ac1695c907b7a79268c2
Mikaela Mckee <mikaelamckee@iMac.local>
2018-07-11 20:01:33 -0500

    Second Commit

---
+++ Assets/JavaScript/app.js
@@ -0,0 +1,51 @@
+
+
+//Create an array of potential answers for each question.
+
+var UserChoices="0";
+var UserLosses="0";
+
+var Questions = [
+      "Q1", // Who has more MVP's during the Finals amongst the following?
+      "Q2", // What Fortune 500 Company just purchased Yahoo?
+      "Q3", // What brand does James Harden sponsor?
+      "Q4", // What professional wrestler played college football at the University of Miami?
+      "Q5" //  Who was the 44th president of the United States of America?<
+];
+
+var answerOptions = [
+    "lebronJames",
+    "kobeBryant",
+    "stephenCurry",
+    "kyrieIrving",
+    "AT&T",
+    "SAMS",
+    "wholeFoods",
+    "Verizon",
+    "Nike",
+    "Addidas",
+    "fruitOftheLoom",
+    "calvinKlein",
+    "stoneColdaustin",
+    "ricFlair",
+    "theRock",
+    "hulkHogan",
+    "barackObama",
+    "billClinton",
+    "georgeWashington",
+    "kanyeWest"
+];
+
+
+
+//if the correct answer is selected, the total score increases.
+
+if(Q1) {
+    
+}
+//if the wrong answer is selected, the total score decreases.
+//Prevent user from selecting multiple answers per questions.
+//Create a Timer 
+//Set the Timer to start when the page loads
+//Set the Timer to Count down
+//Display the user's total score when the time runs out
